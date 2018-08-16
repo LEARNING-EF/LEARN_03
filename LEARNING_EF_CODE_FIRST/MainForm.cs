4a14fc24eabebfb0ceded7ef8b971f51caf5ccfe
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-08-16 11:41:49 +0430

    Add project files.

---
+++ LEARNING_EF_CODE_FIRST/MainForm.cs
@@ -0,0 +1,79 @@
+﻿namespace LEARNING_EF_CODE_FIRST
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+			// *************************************
+			// *** How to use disposable classes ***
+			// *************************************
+
+			// Solution (1)
+			//DisposableClass obj = null;
+
+			//try
+			//{
+			//	obj = new DisposableClass();
+
+			//	// با شیء مربوطه کار می کنيم
+			//}
+			//catch (System.Exception ex)
+			//{
+			//	// خطای مناسب را لاگ کرده و پيغام مناسب را به کاربر نمايش می دهيم
+			//}
+			//finally
+			//{
+			//	if(obj != null)
+			//	{
+			//		obj.Dispose();
+			//		obj = null;
+			//	}
+			//}
+			// /Solution (1)
+
+			// Solution (2)
+			//using (DisposableClass obj =  new DisposableClass())
+			//{
+			//}
+			// /Solution (2)
+
+			Models.DatabaseContext databaseContext = null;
+
+			try
+			{
+				databaseContext =
+					new Models.DatabaseContext();
+
+				Models.Person person = new Models.Person();
+
+				person.Id = 1;
+				person.Age = 40;
+				person.IsSupervisor = true;
+				person.FullName = "Mr. Dariush Tasdighi";
+
+				// بعد از دستور ذيل، در صورتی که بانک اطلاعاتی
+				// وجود نداشته باشد، بانک اطلاعاتی ايجاد می گردد
+				databaseContext.People.Add(person);
+
+				databaseContext.SaveChanges();
+			}
+			catch (System.Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				if (databaseContext != null)
+				{
+					databaseContext.Dispose();
+					databaseContext = null;
+				}
+			}
+		}
+	}
+}
